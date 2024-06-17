cf7e112ff2a788079a19478c3fd60d4142ad61f0
Ivansancor <ivanvaldeolivo95@gmail.com>
2024-06-17 06:43:02 +0200

    created my own version of the pyramid generator

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Pyramid generator
+
+const character = "+";
+const rows = 6;
+let arr = [];
+let inverted = true;
+
+function createHeight(number) {
+    if (inverted) {
+        for (let i = number; i > 0; i--) {
+            for (let j = 1; j <= i; j++) {
+                arr = [];
+                arr.push(character.repeat(j * 2 - 1));     
+            }
+            console.log(" ".repeat(number - i) + arr.toString());
+        }
+    } else {
+        for (let i = 1; i < number + 1; i++) {
+            for (let j = 1; j <= i; j++) {
+                arr = [];
+                arr.push(character.repeat(j * 2 - 1));     
+            }
+            console.log(" ".repeat(number - i) + arr.toString());
+        }
+    }
+}
+
+createHeight(rows);
